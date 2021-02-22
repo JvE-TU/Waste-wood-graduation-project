156332548b3b4710c074ed053cdda285a116ba9d
JvE-TU <79092521+JvE-TU@users.noreply.github.com>
2021-02-22 14:20:33 +0100

    Add files via upload

---
+++ Exterior wall element tool/wood_classes/classes_wood.py
@@ -0,0 +1,39 @@
+﻿
+print('import succes') 
+
+class wood_structural():
+    def __init__(self, length, width, depth, db_id):
+        self.length = length
+        self.width = width
+        self.depth = depth
+        self.id = db_id
+        
+    def print_data(self):
+        print(self.length, self.width, self.depth, self.id)
+
+
+class wood_substructure(wood_structural):
+    pass
+
+
+class wood_cladding():
+    def __init__(self, length, height, depth, db_id):
+        self.length = length
+        self.height = height
+        self.depth = depth
+        self.id = db_id
+
+
+class coordinates_segments():
+    def __init__(self, x_cor_start, z_cor_start, x_cor_end, z_cor_end):
+        self.x_cor_start = x_cor_start
+        self.z_cor_start = z_cor_start
+        self.x_cor_end = x_cor_end
+        self.z_cor_end = z_cor_end
+        
+    def print_cor(self):
+        print(self.x_cor_start, self.z_cor_start, self.x_cor_end, self.z_cor_end)
+
+
+class coordinates_segments_cladding(coordinates_segments):
+    pass
